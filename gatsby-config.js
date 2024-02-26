b60754dc0aeb9ab19b055d1d41b07a87595748a7
Sabarivelan Ganesan <sabarivelangn@gmail.com>
2024-02-26 23:07:08 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Sabarivelan`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
