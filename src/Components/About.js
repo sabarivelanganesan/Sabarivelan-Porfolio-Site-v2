c47a7b921402b3ad65bbdb2cedb8279927594267
Sabarivelan Ganesan <sabarivelangn@gmail.com>
2024-02-27 21:02:59 +0000

    [PATCH] Add Init Commit

---
+++ src/Components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="pr-4 md:pr-40">
+            <h4 className="text-base md:text-lg">Software Developer</h4>
+            <h2 className="text-xl md:text-3xl font-bold">Sabarivelan Ganesan</h2>
+
+            <div className="mt-4">
+                <p className="text-sm md:text-base">
+                    I am a dedicated front-end web developer with three years of experience. My passion lies in creating visually stunning and user-friendly websites that provide exceptional user experiences. My focus is on blending aesthetics and functionality to craft engaging online journeys that captivate and inspire visitors.
+                </p>
+            </div>
+
+            <div className="mt-4">
+                <button className="mr-2 mb-2 md:mb-0">Let's talk</button>
+                <button>Connect on LinkedIn</button>
+            </div>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/Components/Experience.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Experience = () => {
+    return (
+        <div>
+            <h5>Experience</h5>
+            <h3>Here is a quick Summary of my most recent experiences</h3>
+
+            <div>
+                <div>
+                    <h5>Software Developer @ Zoho</h5>
+                    <p>mm yyyy - present</p>
+                </div>
+                <div>
+                    <p>location</p>
+                    <p>link</p>
+                    <p>Description</p>
+                    <ul>
+                        <li>skills in this role</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h5>Software Intern @ Zoho</h5>
+                    <p>mm yyyy - mm  yyyy</p>
+                </div>
+                <div>
+                    <p>location</p>
+                    <p>link</p>
+                    <p>Description</p>
+                    <ul>
+                        <li>skills in this role</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience;
